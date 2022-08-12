86f524a8d93433ba4740590e22c7bdb46743c864
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-12 20:46:32 -0300

    06 - Tipos de dados

--- variaveis.php
+++ variaveis.php
@@ -4,4 +4,12 @@ $idade = 21;
 
 $idadeDaqui10Anos = $idade + 10;
 
-echo $idadeDaqui10Anos;
+$salario = 1000.50;
+
+echo "$idadeDaqui10Anos \n";
+
+echo "\t", gettype($idadeDaqui10Anos), "\n";
+
+echo $salario, "\n";
+
+echo "\t", gettype($salario);
