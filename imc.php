6f81094f82fdda2dbd55017e2be8a4f1b929d6a1
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-09-13 19:35:43 -0300

    Calculo de IMC com php

---
+++ imc.php
@@ -0,0 +1,17 @@
+<?php
+
+$peso = 73;
+$altura = 1.70;
+$imc = $peso/$altura ** 2;
+
+echo "Seu IMC é de $imc. Você está com IMC "; 
+
+if ($imc < 18) {
+    echo "Abaixo";
+} elseif ($imc < 25) {
+    echo "Dentro do esperado";
+} else {
+    echo "Acima";
+} 
+
+echo " do recomendado";
