1e6be4547dec1b093d2b0b96e44da1347d878d35
Paulo Junior <paulo.junior@avanz.com.br>
2022-09-20 19:39:35 -0300

    Conhecendo o Twig

--- symfony/controle_series_symfony/src/Controller/SeriesController.php
+++ symfony/controle_series_symfony/src/Controller/SeriesController.php
@@ -3,14 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SeriesController extends AbstractController
 {
     #[Route('/series', name: 'app_series')]
-    public function seriesList(): Response
+    public function index(): Response
     {
         $seriesList = [
             'Alf',
@@ -19,11 +18,8 @@ class SeriesController extends AbstractController
             'Digaloo',
             'Suits'
         ];
-        $html = '<ul>';
-        foreach ($seriesList as $series) {
-            $html .= '<li>'.$series.'</li>';
-        }
-        $html .= '</ul>';
-        return new JsonResponse($seriesList);
+        return $this->render('series/index.html.twig', [
+            'seriesList' => $seriesList,
+        ]);
     }
 }
