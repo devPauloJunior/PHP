2ebb7c45bfe60f3903f65432c5cc88623d67f84a
Paulo Junior <paulo.junior@avanz.com.br>
2022-09-20 16:16:27 -0300

    Lidando com Request e Response

--- symfony/controle_series_symfony/src/Controller/OlamundoController.php
+++ symfony/controle_series_symfony/src/Controller/OlamundoController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OlamundoController
 {
-    #[Route('/ola_mundo', name: 'app_olamundo')]
-    public function index(): Response
+    #[Route('/ola_mundo')]
+    public function index(Request $request): Response
     {
-        return new Response(content:'<h1>Olá Mundo</h1>');
+        return new Response(
+            content:'<h1>ID:</h1>' . $request->query->get(key:'id'),
+            status:401
+        );
     }
 }
